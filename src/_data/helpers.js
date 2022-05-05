b391ee07007ca0f2bd1c3389ed59ba6ada0bbfcb
MinhKhangTran <minhkhang.tran@gmx.de>
2022-05-05 06:44:27 +0200

    :sparkles: fix navbar issues and add pages

--- src/_data/helpers.js
+++ src/_data/helpers.js
@@ -3,9 +3,14 @@ module.exports = {
   getLinkActiveState(itemUrl, pageUrl) {
     let response = "";
 
-    if (itemUrl.length > 0 && pageUrl.indexOf(itemUrl) === 0) {
+    if (itemUrl === pageUrl) {
+      response = ' aria-current="page"';
+    }
+
+    if (itemUrl.length > 1 && pageUrl.indexOf(itemUrl) === 0) {
       response += ' data-state="active"';
     }
+
     return response;
   },
 };
