6bbb20f6091bfc0dc01ad738ab5a5540f568574e
MinhKhangTran <minhkhang.tran@gmx.de>
2022-08-05 06:05:51 +0200

    :sparkles: next and prev button + back to top

--- src/js/index.js
+++ src/js/index.js
@@ -36,3 +36,17 @@ toggleBtn.addEventListener("click", () => {
     disableDarkMode();
   }
 });
+
+// Up to top
+
+// Selector
+const upButton = document.querySelector(".up");
+
+// appear if scrolled 350
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= 290) {
+    upButton.classList.add("show");
+  } else {
+    upButton.classList.remove("show");
+  }
+});
